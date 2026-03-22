2c05690354d429e95aab3ef6d94e5b982539321d
Peter Neiss <neiss@plasus.de>
2026-03-22 14:33:50 +0100

    See if it helps older gcc

--- include/bound/detail/debug.hpp
+++ include/bound/detail/debug.hpp
@@ -17,8 +17,29 @@ namespace bnd
   template <auto...N>
   struct print_values;
 
+  namespace 
+  {
+  // older gcc workaround (gcc 12.2)
+  [[noreturn]] void throw_overflow_rt(const char* what)
+  {
+      throw std::overflow_error{what};
+  }
+  } 
+
+  constexpr void OVERFLOW_trap(const char* what)
+  {
+    if consteval
+    { throw_overflow_rt(what); } // not constexpr → compile error
+    else
+    { throw_overflow_rt(what); } // runtime throw
+  }
+/*
   constexpr void OVERFLOW_trap(char const* what = "overflow") 
-  { throw std::overflow_error{what}; }
+  { 
+    if not conste
+    throw std::overflow_error{what}; 
+  }
+*/
 } // namespace bnd
 
 #endif // BNDdebugHPP
